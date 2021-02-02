358f329c18b45fc271906244c1c0ef63f7e6bb5d
BW Oh <byoungwoo5@naver.com>
2021-02-02 19:53:26 +0000

    ✨ Success table

--- gunpla/review/views.py
+++ gunpla/review/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
+import urllib.request
+import json
 
 
 def index(request):
-    return render(request, "review/index.html")
+    url = 'https://gunpladb-noovf.run.goorm.io/api/join'
+    res = urllib.request.urlopen(url)
+    res_body = res.read()
+    data = json.loads(res_body.decode("utf-8"))
+    # print(data[0])
+    return render(request, "review/index.html", {'data': data})
